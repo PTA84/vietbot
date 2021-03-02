0e6095ddeafeaaedb15ef2889efa50dfaad931ad
Nguyễn Duy <64348125+phanmemkhoinghiep@users.noreply.github.com>
2021-03-02 14:47:49 +0700

    Delete index.php

--- index.php
+++
@@ -1,6 +0,0 @@
-<form action="program.php" method="post">
-    STT_Engine:<br><input type="text" name="stt_engine" value="Google Cloud"><br>
-    TTS Engine:<br><input type="text" name="tts_engine" value="Zalo"><br>
-    Hass Url:<br><input type="text" name="hass_url" value= "192.168.1.1"><br>
-    <input type="submit" id ="submitButton" value="Submit">
-</form>
