0b8bf12f4b94e49c7d6e1ae2d17619e5486dbb35
Nguyễn Duy <64348125+phanmemkhoinghiep@users.noreply.github.com>
2021-03-02 13:58:55 +0700

    Delete checkser.py

--- checkser.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-service = "bot"
-def check_bot():
-    p =  subprocess.Popen(["systemctl", "is-active",  service], stdout=subprocess.PIPE)
-    (output, err) = p.communicate()
-    output = output.decode('utf-8')
-
-    return output
