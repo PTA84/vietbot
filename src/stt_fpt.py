11aa81152144596378336463ce1c494c8d5e1f3c
Nguyễn Duy <64348125+phanmemkhoinghiep@users.noreply.github.com>
2021-03-07 19:17:05 +0700

    Update stt_fpt.py

--- src/stt_fpt.py
+++ src/stt_fpt.py
@@ -1,30 +1,9 @@
 #!/usr/bin/python3
 # Requires PyAudio.
 # -*- coding: utf-8 -*-
-# from helper import *
-# import gih
-# import chsv
-# import process
-#import hass_onoff
-# import random
-# import speaking
-# import ptz
-# import time
-# import amlich
-# import thu
-# import ngayle
-# import tintuc
-# import loto 
-# import fun 
-# import hass_execute
-# import re 
-# import radio 
-# import weth
-# import wk
-# import spot
-# import speech_recognition as sr
-# import weather as wt
-# from pygame import mixer
+from termcolor import colored
+import gih
+api_fpt_key =gih.get_config('fpt_api')
 
 def main():
 #   print ('FPT')
@@ -37,9 +16,6 @@ def main():
     with open('tmp/record.wav', "wb") as f:
         f.write(audio.get_wav_data())
     data = ""
-    keys = ['Os1MlynwG2W7eNFecfME0EOHZ9hCcobk','hFewll7iiYesYsc5TQO9RyPWogvYGXff','1E9dz9Oyax0WFbqVxIsBQSTuCf04yA6I']
-    api_fpt_key = random.choice(keys)
-    print (api_fpt_key)
     try:
         import requests
         url = 'https://api.fpt.ai/hmi/asr/general'
@@ -53,7 +29,7 @@ def main():
             data = element['utterance'] 
 #   print(response.json())
 #       data = r.recognize_google(audio, language = "vi-VN")
-        print('BOT nghe được: '+ data)
+        print(colored('[BOT-STT-FPT]: '+data,'green'))
     except sr.UnknownValueError:    
         print('Sao bạn không nói gì')
         data ='ĐƯỢC RỒI'
