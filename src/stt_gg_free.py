c4099413f99e24af9e55624d1db7ba2690fab25f
Nguyễn Duy <64348125+phanmemkhoinghiep@users.noreply.github.com>
2021-04-23 18:30:59 +0700

    Update stt_gg_free.py

--- src/stt_gg_free.py
+++ src/stt_gg_free.py
@@ -1,47 +1,32 @@
-#!/usr/bin/python3
-# Requires PyAudio.
-# -*- coding: utf-8 -*-
-# from helper import *
-# import gih
-# import chsv
-# import process
-# import hass_onoff
-# import random
-# import speaking
-# import ptz
-# import time
-# import amlich
-# import thu
-# import ngayle
-# import tintuc
-# import loto 
-# import fun 
-# import execute
-# import re 
-# import radio 
-# import weth
-# import wk
-# import spot
-# import weather as wt
-# from pygame import mixer
+import time
+import json
+import speech_recognition as sr
+from termcolor import colored
+STREAMING_LIMIT=None
+with open('config.json') as config_json:
+    config_data = json.load(config_json)
+for p in config_data['stt_engine']:
+    if p['name'] == 'stt_gg_free':
+        STREAMING_LIMIT=p['time_out']
+def get_current_time():
+    return int(round(time.time() * 1000))
+
 
 def main():
-    import speech_recognition as sr
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source, duration=1)
-        audio = r.listen(source)
-    data = ""
-    try:
-        data = r.recognize_google(audio, language = "vi-VN")
-        print('BOT nghe được: '+ data)
-    except sr.UnknownValueError:    
-        print('Sao bạn không nói gì')
-        data ='ĐƯỢC RỒI'
-        pass
-    except sr.RequestError as e:
-        print('èo èo'+ e)
-        pass
+    start_time=get_current_time()        
+    data=None
+    while get_current_time() - start_time < STREAMING_LIMIT:    
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            try:
+                r.adjust_for_ambient_noise(source, duration=1)
+                audio = r.listen(source)
+                data = r.recognize_google(audio, language = "vi-VN")
+                if len(data) >0:
+                    print(colored('[HUMAN]: '+data,'blue'))            
+                    break
+            except:
+                pass
     return data
 
 
